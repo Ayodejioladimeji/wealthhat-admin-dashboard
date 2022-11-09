a16b75d25f5673b3c9bf5188588af78d7ae414c8
Ayodejioladimeji <layobright11@gmail.com>
2022-11-09 08:38:08 +0100

    implemented the login components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 // Components
-import Router from './routes/Router';
-import ScrollToTop from './common/ScrollToTop';
-import Alert from './common/alert/Alert';
-import { all_users } from './redux/actions/usersAction';
-import './App.css';
+import Router from "./routes/Router";
+import ScrollToTop from "./common/ScrollToTop";
+import Alert from "./common/alert/Alert";
+import { loggedAgent } from "./redux/actions/usersAction";
+import "./App.css";
 
 function App() {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
 
   // useEffect
+
+  // get logged in agents
   useEffect(() => {
     if (token) {
-      dispatch(all_users(token));
+      dispatch(loggedAgent(token));
     }
-  }, [dispatch]);
+  }, [token, dispatch]);
 
   //
   return (
